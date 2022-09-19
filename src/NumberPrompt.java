d6d72c7ffc7532046b196630eeaacc05d020c7d1
anybuddy <fuaw.gc@LST.de>
2022-09-19 15:07:42 +0200

    - condense "UI2" and "UI3" into "NumberPrompt"

--- src/NumberPrompt.java
+++ src/NumberPrompt.java
@@ -4,26 +4,27 @@ import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
-public class UI2 {
+public class NumberPrompt {
 
-    public void open() {
+    public void open(int number) {
         Stage stage = new Stage();
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("UI2.fxml")));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("NumberPrompt.fxml")));
             Scene scene = new Scene(root);
             stage.setTitle("Trainingsspiel");
             stage.setScene(scene);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setResizable(false);
-        } catch (Exception e) {
-            System.out.println("UI2-couldn't be initialized: \n" + e);
+        } catch (IllegalStateException | IOException e) {
+            System.out.println("`NumberPrompt` couldn't be initialized: \n" + e);
         }
         try {
             stage.show();
-        } catch (Exception e) {
-            System.out.println("UI2-couldn't be opened: \n" + e);
+        } catch (IllegalStateException e) {
+            System.out.println("`NumberPrompt` couldn't be opened: \n" + e);
         }
     }
 }
